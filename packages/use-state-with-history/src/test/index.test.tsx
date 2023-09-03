b73d67386799f2593694400b8a02e70739a837cf
ibrahimshamma99 <i.abushammah@gmail.com>
2023-09-03 20:08:19 +0300

    unit test use-state-with history

--- packages/use-state-with-history/src/test/index.test.tsx
+++ packages/use-state-with-history/src/test/index.test.tsx
@@ -18,4 +18,88 @@ describe("Counter", () => {
     expect(result.current[0]).toBe(1);
     await act(() => promise);
   });
+
+  test("increments by one two times start with zero", async () => {
+    const promise = Promise.resolve();
+    const { result } = renderHook(() => useStateWithHistory<number>(0));
+    act(() => {
+      result.current[1](result.current[0] + 1);
+    });
+
+    act(() => {
+      result.current[1](result.current[0] + 1);
+    });
+    expect(result.current[0]).toBe(2);
+    await act(() => promise);
+  });
+
+  test("increments by one two times start with zero then goes back by 1 step", async () => {
+    const promise = Promise.resolve();
+    const { result } = renderHook(() => useStateWithHistory<number>(0));
+    act(() => {
+      result.current[1](result.current[0] + 1);
+    });
+    act(() => {
+      result.current[1](result.current[0] + 1);
+    });
+
+    act(() => {
+      result.current[2].backward();
+    });
+    expect(result.current[0]).toBe(1);
+    await act(() => promise);
+  });
+
+  test("increments by one two times start with zero then goes back by 2 steps and one forward", async () => {
+    const promise = Promise.resolve();
+    const { result } = renderHook(() => useStateWithHistory<number>(0));
+    act(() => {
+      result.current[1](result.current[0] + 1);
+    });
+    act(() => {
+      result.current[1](result.current[0] + 1);
+    });
+
+    act(() => {
+      result.current[2].backward();
+    });
+
+    act(() => {
+      result.current[2].backward();
+    });
+
+    act(() => {
+      result.current[2].forward();
+    });
+    expect(result.current[0]).toBe(1);
+    await act(() => promise);
+  });
+
+  test("increments by one two times start with zero then goes back by 2 steps and one forward then goes to 0", async () => {
+    const promise = Promise.resolve();
+    const { result } = renderHook(() => useStateWithHistory<number>(0));
+    act(() => {
+      result.current[1](result.current[0] + 1);
+    });
+    act(() => {
+      result.current[1](result.current[0] + 1);
+    });
+
+    act(() => {
+      result.current[2].backward();
+    });
+
+    act(() => {
+      result.current[2].backward();
+    });
+
+    act(() => {
+      result.current[2].forward();
+    });
+    act(() => {
+      result.current[2].go(0);
+    });
+    expect(result.current[0]).toBe(0);
+    await act(() => promise);
+  });
 });
